77a59d213911d0f3bb1689378a36eb4fe1fccc99
QuardSA <smirnov_aleksey04@inbox.ru>
2023-12-05 13:56:21 +0500

    card add

--- resources/views/hotelcard.blade.php
+++ resources/views/hotelcard.blade.php
@@ -1,16 +1,106 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
-	<x-link></x-link>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <x-link></x-link>
 </head>
+
 <body>
-	<x-header></x-header>
-        <div class="container">
-        
+    <x-header></x-header>
+    <div class="container mt-3">
+        <div class="slider-container">
+
         </div>
+        <div class="info-hotel-card d-flex">
+            <div class="info" style="max-width: 65%;">
+                <p class="description">
+                    Оригинальная и очень самобытная гостиница “Поляна 1389 Отель и Спа” открылась на высокогорном плато
+                    Псехако курортного местечка Эсто-Садок прямо накануне зимних Олимпийских Игр, которые достойно
+                    принимал
+                    Сочи в 2014 году.
+
+                    Если вам по душе уютный комфорт эко стиля и вы можете вечно любоваться на горные красоты
+                    Краснодарского
+                    края и вдыхать его кристально чистый воздух, то вам дорога в этот отель.
+
+                    Фешенебельная семейная гостиница Поляна 1389 Отель и Спа, выполненная в стилистике альпийских шале,
+                    порадует вас разноплановыми возможностями размещения как в 151 номере и 70 апартаментах базовых
+                    корпусов, так и в отдельно стоящих 28 прекрасно укомплектованных коттеджах.
+
+                    Гастрономический кластер Поляна 1389 Отель и Спа представлен филиалом известного сочинского
+                    ресторана
+                    “Мясной синдикат” и видовым рестораном “Панорама”, работающим в формате “шведского стол”. Дух
+                    захватывает от великолепной географии плавательного пространства с сообщающимся открытым и крытым
+                    бассейном с термальной зоной, купелями, каскадными душами и несколькими парными комнатами.
+
+                    Заслуживает уважения и многопрофильный комплекс услуг спа-центра Thalgo. Конференц-площадки отеля
+                    подойдут для проведения корпоративных мероприятий любого формата.
+                </p>
+                <h3 class="text-dark fw-semibold">Услуги и удобства</h3>
+                <div class="services-amenities__body d-flex flex-wrap">
+                    <section class="services-amenities__item">
+                        <ul class="services-amenities__list">
+                            <p class="fs-3 fw-semibold m-0">Общее</p>
+                            <li class="services-amenities__list-item list-group-item fs-5 fw-medium my-2"><img
+                                    src="/img/Bookmark.svg">Ресепшн 24 часа </li>
+                            <li class="services-amenities__list-item list-group-item fs-5 fw-medium my-2"><img
+                                    src="/img/Bookmark.svg">Парковка на территории </li>
+                            <li class="services-amenities__list-item list-group-item fs-5 fw-medium my-2"><img
+                                    src="/img/Bookmark.svg">Бизнес центр</li>
+                            <li class="services-amenities__list-item list-group-item fs-5 fw-medium my-2"><img
+                                    src="/img/Bookmark.svg">Охрана 24 часа</li>
+                        </ul>
+                    </section>
+                    <section class="services-amenities__item">
+                        <ul class="services-amenities__list">
+                            <p class="fs-3 fw-semibold m-0">Спорт и SPA</p>
+                            <li class="services-amenities__list-item list-group-item fs-5 fw-medium my-2"><img
+                                    src="/img/Bookmark.svg">Ресепшн 24 часа </li>
+                            <li class="services-amenities__list-item list-group-item fs-5 fw-medium my-2"><img
+                                    src="/img/Bookmark.svg">Парковка на территории </li>
+                            <li class="services-amenities__list-item list-group-item fs-5 fw-medium my-2"><img
+                                    src="/img/Bookmark.svg">Бизнес центр</li>
+                            <li class="services-amenities__list-item list-group-item fs-5 fw-medium my-2"><img
+                                    src="/img/Bookmark.svg">Охрана 24 часа</li>
+                        </ul>
+                    </section>
+                    <section class="services-amenities__item">
+                        <ul class="services-amenities__list">
+                            <p class="fs-3 fw-semibold m-0">Спорт и SPA</p>
+                            <li class="services-amenities__list-item list-group-item fs-5 fw-medium my-2"><img
+                                    src="/img/Bookmark.svg">Ресепшн 24 часа </li>
+                            <li class="services-amenities__list-item list-group-item fs-5 fw-medium my-2"><img
+                                    src="/img/Bookmark.svg">Парковка на территории </li>
+                            <li class="services-amenities__list-item list-group-item fs-5 fw-medium my-2"><img
+                                    src="/img/Bookmark.svg">Бизнес центр</li>
+                            <li class="services-amenities__list-item list-group-item fs-5 fw-medium my-2"><img
+                                    src="/img/Bookmark.svg">Охрана 24 часа</li>
+                        </ul>
+                    </section>
+                </div>
+                <div class="conditions mt-3">
+                    <h3 class="text-dark fw-semibold">Условия размещения</h3>
+                    <div class="conditions-body">
+                        <h4 class="text-dark fw-semibold">Заезд</h4>
+                        <p>С 15:00 до 00:00</p>
+                        <h4 class="text-dark fw-semibold">Отъезд</h4>
+                        <p>С 00:00 до 12:00</p>
+                    </div>
+                </div>
+            </div>
+            <div class="main-info-hotel border ms-4 shadow p-3 mb-5 bg-body-tertiary rounded"
+                style="max-height: 10rem; width:30%;">
+                <p class="text-dark fw-semibold fs-5">Поляна 1389 Отель и Спа</p>
+                <p class="p text-dark fw-semibold fs-5 mb-0">6 - 7 Декабрь</p>
+                <p class="text-dark fw-light">Заезд с 15:00, отъезд до 12:00</p>
+            </div>
+        </div>
+    </div>
     <x-footer></x-footer>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 </body>
+
 </html>
